a254a631a82656dcc383af4e6abfcf9b2a8b478f
Ryan Cooper <ryancooper@Ryans-MacBook-Pro.local>
2018-10-12 17:41:25 -0600

    Fixed margin at bottom

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -4,32 +4,34 @@ import { connect } from 'react-redux';
 import selectJobs from '../selectors/jobs';
 
 const JobList = (props) => (
-    <div className="content-container">
-        {props.jobs.length === 0 ? (
-            <div>
-                <span>No Jobs</span> 
-            </div>           
-        ) : (
-            <table className="table">
-                <thead className="table-head">
-                    <tr>
-                        <td>Company</td>
-                        <td>Position</td>
-                        <td>Salary</td>
-                        <td>Next Interview Type</td>
-                        <td>Status</td>
-                        <td></td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {props.jobs.map((job) => {
-                        return <JobListItem key={job.id} job={job} />
-                    })}
-                </tbody>
-            </table>
-        )
-    
-        }
+    <div className="table-whole">
+        <div className="content-container">
+            {props.jobs.length === 0 ? (
+                <div>
+                    <span>No Jobs</span> 
+                </div>           
+            ) : (
+                <table className="table">
+                    <thead className="table-head">
+                        <tr>
+                            <td>Company</td>
+                            <td>Position</td>
+                            <td>Salary</td>
+                            <td>Next Interview Type</td>
+                            <td>Status</td>
+                            <td></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {props.jobs.map((job) => {
+                            return <JobListItem key={job.id} job={job} />
+                        })}
+                    </tbody>
+                </table>
+            )
+        
+            }
+        </div>
     </div>
 );
 
