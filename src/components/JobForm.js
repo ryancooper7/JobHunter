95fbb376ae89d7872954a70f5e39b6b1003d3968
Ryan Cooper <ryancooper@Ryans-MacBook-Pro.local>
2018-10-14 23:55:18 -0600

    More select styling

--- src/components/JobForm.js
+++ src/components/JobForm.js
@@ -1,93 +1,107 @@
-import React from 'react';
+import React from "react";
+import Select from "react-select";
+
+const options = [
+  { value: "screen", label: "Initial Screen" },
+  { value: "phone", label: "Phone Interview" },
+  { value: "video", label: "Video Conference" },
+  { value: "inPerson", label: "In Person Interview" }
+];
 
 export default class JobForm extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            title: props.job ? props.job.title : '',
-            company: props.job ? props.job.company : '',
-            salary: props.job ? props.job.salary : '',
-            interview: props.job ? props.job.interview : 'placeholder',
-            error:''
-        };
+    this.state = {
+      title: props.job ? props.job.title : "",
+      company: props.job ? props.job.company : "",
+      salary: props.job ? props.job.salary : "",
+      interview: props.job ? props.job.interview : "placeholder",
+      error: ""
+    };
+  }
+
+  onTitleChange = e => {
+    const title = e.target.value;
+    this.setState(() => ({ title }));
+  };
+
+  onCompanyChange = e => {
+    const company = e.target.value;
+    this.setState(() => ({ company }));
+  };
+
+  onSalaryChange = e => {
+    const salary = e.target.value;
+    this.setState(() => ({ salary }));
+  };
+
+  onInterviewChange = newInterview => {
+    const interview = newInterview.value;
+    this.setState(() => ({ interview }));
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    if (
+      !this.state.title ||
+      !this.state.company ||
+      !this.state.salary ||
+      this.state.interview == "placeholder"
+    ) {
+      this.setState(() => ({
+        error: "Please provide all the necessary infromation"
+      }));
+    } else {
+      this.setState(() => ({
+        error: ""
+      }));
+      this.props.onSubmit({
+        title: this.state.title,
+        company: this.state.company,
+        salary: this.state.salary,
+        interview: this.state.interview,
+        status: this.props.job ? this.props.job.status : "interviewing"
+      });
     }
+  };
 
-    onTitleChange = (e) => {
-        const title = e.target.value;
-        this.setState(() => ({title}));
-    };
-
-    onCompanyChange = (e) => {
-        const company = e.target.value;
-        this.setState(() => ({company}));
-    };
-
-    onSalaryChange = (e) => {
-        const salary = e.target.value;
-        this.setState(() => ({salary}));
-    };
-
-    onInterviewChange = (e) => {
-        const interview = e.target.value;
-        this.setState(() => ({interview}));
-    }
-
-    onSubmit = (e) => {
-        e.preventDefault();
-        if (!this.state.title || !this.state.company || !this.state.salary || (this.state.interview == 'placeholder')) {
-            this.setState(() => ({
-                error: 'Please provide all the necessary infromation'
-            }));
-        } else {
-            this.setState(() => ({
-                error: ''
-            }));
-            this.props.onSubmit({
-                title: this.state.title,
-                company: this.state.company,
-                salary: this.state.salary,
-                interview: this.state.interview,
-                status: this.props.job ? this.props.job.status : 'interviewing'
-            });
-        }
-    };
-
-    render() {
-        return (
-            <form className="form" onSubmit={this.onSubmit}>
-                {this.state.error && <p>{this.state.error}</p>}
-                <input 
-                    className="text-input"
-                    type="text" 
-                    placeholder="Job Title"
-                    value ={this.state.title}
-                    onChange={this.onTitleChange}
-                />
-                <input 
-                    className="text-input"
-                    type="text"
-                    placeholder="Company"
-                    value={this.state.company}
-                    onChange={this.onCompanyChange}
-                />
-                <input 
-                    className="text-input"
-                    type="text"
-                    placeholder="Salary"
-                    value={this.state.salary}
-                    onChange={this.onSalaryChange}
-
-                />
-                <select className="select select-form" onChange={this.onInterviewChange} value={this.state.interview}>
-                    <option disabled value="placeholder">Next Interview</option>
-                    <option value="screen">Initial Screen</option>
-                    <option value="phone">Phone Interview</option>
-                    <option value="video">Video Conference</option>
-                    <option value="inPerson">In Person Interview</option>
-                </select>
-                <button className="button-form">{this.props.job ? 'Update Job' : 'Add Job'}</button>
-            </form>
-        );
-    }
-};
+  render() {
+    return (
+      <form className="form" onSubmit={this.onSubmit}>
+        {this.state.error && <p>{this.state.error}</p>}
+        <input
+          className="text-input"
+          type="text"
+          placeholder="Job Title"
+          value={this.state.title}
+          onChange={this.onTitleChange}
+        />
+        <input
+          className="text-input"
+          type="text"
+          placeholder="Company"
+          value={this.state.company}
+          onChange={this.onCompanyChange}
+        />
+        <input
+          className="text-input"
+          type="text"
+          placeholder="Salary"
+          value={this.state.salary}
+          onChange={this.onSalaryChange}
+        />
+        <Select
+          className="form-select"
+          onChange={this.onInterviewChange}
+          value={options.find(option => this.state.interview === option.value)}
+          options={options}
+          placeholder="Next Interview Type"
+        />
+        <button className="button-form">
+          {this.props.job ? "Update Job" : "Add Job"}
+        </button>
+      </form>
+    );
+  }
+}
